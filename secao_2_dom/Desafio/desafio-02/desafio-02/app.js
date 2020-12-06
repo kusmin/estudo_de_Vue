41f589fafc04ea421bb8ca6d8040bc9a31d8a05c
renan <kusmin00@hotmail.com>
2020-12-06 13:44:28 -0300

    'feito exercicio 2'

--- secao_2_dom/Desafio/desafio-02/desafio-02/app.js
+++ secao_2_dom/Desafio/desafio-02/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        exibirAlerta() {
+            alert('botao clicado')
+        },
+        atualizarValor(event) {
+            this.valor = event.target.value
+        }
     }
 })
