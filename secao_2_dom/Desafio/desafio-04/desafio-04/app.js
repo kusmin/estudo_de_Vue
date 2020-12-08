e25dd038e4869f6ddb11789911b36ef0beb5820e
renan <kusmin00@hotmail.com>
2020-12-08 16:41:23 -0300

    fechando a secao 2

--- secao_2_dom/Desafio/desafio-04/desafio-04/app.js
+++ secao_2_dom/Desafio/desafio-04/desafio-04/app.js
@@ -1,14 +1,31 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		classe1: 'destaque',
+		classeCSS: 'azul',
+		classe: '',
+		verdadeiro: true,
+		classe2: {
+			width: '300px',
+			height: '400px',
+		},
+		cor2: ''
 	},
 	methods: {
 		iniciarEfeito() {
-			setInterval()
+			setInterval(() => {
+				this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque'
+			}, 3000)
 		},
 		iniciarProgresso() {
 
+		},
+		setVerdadeiro(event) {
+			if(event.target.value == "true"){
+				this.verdadeiro = true
+			}else if(event.target.value == "false"){
+				this.verdadeiro = false
+			}
 		}
 	}
 })
