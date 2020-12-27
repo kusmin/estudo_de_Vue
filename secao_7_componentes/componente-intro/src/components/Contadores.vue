6b789ce6e0f205d9084fa8f78691ad4c7849ab16
kusmin <kusmin00@hotmail.com>
2020-12-27 11:53:47 -0300

    fechando modulo 7

---
+++ secao_7_componentes/componente-intro/src/components/Contadores.vue
@@ -0,0 +1,16 @@
+<template >
+    <div class="contadores">
+        <app-contador v-for="contador in 5" :key="contador"/>
+    </div>
+</template>
+<script>
+import Contador from './Contador.vue'
+export default {
+    components: { 'app-contador': Contador}
+}
+</script>
+<style>
+
+
+
+</style>
